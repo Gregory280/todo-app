92057a150cac3e714428231f1c65d4228d68f690
Gregory N. Mayer <gregory280@hotmail.com>
2020-08-05 19:13:02 -0300

    uPdate

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   root 'pages#home'
-  get 'pages/about'
+  get 'pages/about', to: 'pages#about'
   resources :todo_lists do
     resources :todo_items
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
