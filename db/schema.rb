9ffb0ea420bca2e863a46a0e803f8770fa95cd62
Gregory N. Mayer <gregory280@hotmail.com>
2020-08-05 19:43:28 -0300

    uPDATE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_041738) do
+ActiveRecord::Schema.define(version: 2020_08_05_131847) do
 
   create_table "todo_items", force: :cascade do |t|
     t.text "description"
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(version: 2020_08_05_041738) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "todo_items", "todo_lists"
