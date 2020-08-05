789a8c62a918e64a8205b6aafcae3af69b75352c
Gregory N. Mayer <gregory280@hotmail.com>
2020-08-05 01:47:24 -0300

    Add association between users and todo lists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :todo_lists
+  validates :username, presence: true, length: { minimum: 3, maximum: 20}, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 100 }, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  validates :password, confirmation: true
 end
