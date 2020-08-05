1300055dbfc790571ff1d254867545a1b9d4cb88
Gregory N. Mayer <gregory280@hotmail.com>
2020-08-05 01:30:00 -0300

    Add User scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_202941) do
+ActiveRecord::Schema.define(version: 2020_08_05_041738) do
 
   create_table "todo_items", force: :cascade do |t|
     t.text "description"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_07_28_202941) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
